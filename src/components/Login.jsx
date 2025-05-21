8d87e426cd6d2df897d559f4976e084d7e624475
manpreetsingh153 <www.mannusingh.2002@gmail.com>
2025-05-21 15:42:44 +0530

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -45,7 +45,6 @@ const Login = () => {
         <form className="form" onSubmit={handleSubmit}>
             <input type="text" name="identifier" placeholder="Username or Email" onChange={handleChange} required />
             <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-            <div className="g-recaptcha" data-sitekey="your_site_key"></div>
             <div className='mt-1'>
                 <ReCAPTCHA ref={recaptcha} sitekey={'6Le70EIrAAAAAP8is3TaeHTRotpfZLxm9hc1vcze'} />
             </div>
