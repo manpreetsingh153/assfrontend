9474b3ee252cd04539c6f3211a9b49ee5ac4a071
manpreetsingh153 <www.mannusingh.2002@gmail.com>
2025-05-21 15:24:08 +0530

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,19 +2,42 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useRef } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 const Login = () => {
     const [form, setForm] = useState({ identifier: '', password: '' });
     const [message, setMessage] = useState('');
     const navigate = useNavigate();
     const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
-    
+    const recaptchaRef = useRef(null);
+    const [token, setToken] = useState("");
+    const recaptcha = useRef();
+    useEffect(() => {
+        if (window.grecaptcha) {
+            window.grecaptcha.ready(() => {
+                window.grecaptcha.render(recaptchaRef.current, {
+                    sitekey: "6Le70EIrAAAAAP8is3TaeHTRotpfZLxm9hc1vcze",
+                });
+            });
+        }
+    }, []);
     const handleSubmit = async (e) => {
         e.preventDefault();
-       
+        const captchaValue = recaptcha.current.getValue()
+        if (!captchaValue) {
+            setMessage('Please verify the reCAPTCHA!');
+            // alert('Please verify the reCAPTCHA!')
+        }
+        const recaptchaToken = window.grecaptcha.getResponse();
+        if (!recaptchaToken) {
+            alert("Please complete the reCAPTCHA");
+            return;
+        }
+        setToken(recaptchaToken);
         try {
-             const res = await axios.post(
+            const res = await axios.post(
                 '/api/login',
-                { ...form },
+                { ...form, token: recaptchaToken },
                 {
                     headers: { 'Content-Type': 'application/json' },
                     withCredentials: true
@@ -38,6 +61,9 @@ const Login = () => {
             <input type="text" name="identifier" placeholder="Username or Email" onChange={handleChange} required />
             <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
             <div className="g-recaptcha" data-sitekey="your_site_key"></div>
+            <div className='mt-1'>
+                <ReCAPTCHA ref={recaptcha} sitekey={'6Le70EIrAAAAAP8is3TaeHTRotpfZLxm9hc1vcze'} />
+            </div>
             <button type="submit">Login</button>
             <div>Don't have an account? <Link to="/">Register</Link></div>
             <p>{message}</p>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 
 const Register = () => {
@@ -8,6 +9,8 @@ const Register = () => {
     const [message, setMessage] = useState('');
     const navigate = useNavigate();
     const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
+    const recaptcha = useRef();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -22,7 +25,7 @@ const Register = () => {
             if (res.status === 200) {
                 setMessage('Registration successful. Please log in.');
                 setTimeout(() => {
-                    
+
                     navigate('/login');
                 }, 2000);
             }
@@ -38,6 +41,9 @@ const Register = () => {
             <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
             <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
             <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+            <div className='mt-1'>
+                <ReCAPTCHA ref={recaptcha} sitekey={'6Le70EIrAAAAAP8is3TaeHTRotpfZLxm9hc1vcze'} />
+            </div>
             <button type="submit">Register</button>
             <div>Already have an account? <Link to="/login">Login</Link></div>
             <p>{message}</p>
